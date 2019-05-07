3914baddba7418075b257499b12ecb3b113abdb3
lmh96 <loganhill23@gmail.com>
2019-05-07 01:26:07 -0400

    first commit

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,35 @@
+const friends = require("../data/friends");
+
+module.exports.getter = function(app) {
+    app.get("/api/friends", function(req, res) {
+        let arr = friends.friendsArr;
+
+        res.json(arr);
+    });
+}
+
+module.exports.poster = function(app) {
+    app.post("/api/friends", function(req, res) {
+        let newFriend = req.body;
+
+        let friend;
+        let bestScore = 1000000;
+
+        for(let i = 0; i < friends.friendsArr.length; i++) {
+            let score = 0;
+            for(let j = 0; j < friends.friendsArr[i].answers.length; j++) {
+                let newAns = newFriend.answers[j];
+                let friendAns = friends.friendsArr[i].answers[j];
+
+                score += Math.abs(newAns - friendAns);
+            }
+
+            if(score < bestScore) {
+                bestFriend = friends.friendsArr[i];
+            }
+        }
+
+        friends.add(newFriend);
+        res.json(friend);
+    })
+}
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+
+const api = require("./app/routing/apiRoutes");
+const htm = require("./app/routing/htmlRoutes");
+
+let app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+api.getter(app);
+api.poster(app);
+
+htm.survey(app);
+htm.home(app);
+
+app.listen(PORT || process.env.PORT, function() {
+    console.log("connected to port " + PORT);
+})
